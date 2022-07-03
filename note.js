f22b9266b4e06fba34289a731cc5d7bef07ee419
debo <ogunweoluwadebo@gmail.com>
2022-07-03 15:43:04 +0100

    display user detail

--- note.js
+++ note.js
@@ -1,7 +1,7 @@
-NOTE = [];
+details = [];
 const save = () => {
     if (localStorage.member) {
-        NOTE = JSON.parse(localStorage.member)
+        details = JSON.parse(localStorage.member)
     }
     var userTitle = title.value;
     var userNote = note.value;
@@ -9,18 +9,19 @@ const save = () => {
         title:userTitle,
         note:userNote,
     }
-    NOTE.push(informatio)
-    localStorage.member = JSON.stringify(NOTE);
-    // NOTE = JSON.parse(localStorage.getItem("member"));
-    // let tit = "";
-    // let bod = "";
-    // for (let i = 0; i < NOTE.length; i++) {
-    //     tit += `<h2 class="note_title" id="notetitle">${NOTE[i].title}</h2>`
-    //     bod += `<p class="note_body" id="notebody">${NOTE[i].note}</p>`
-    //     // tit += "<td>"+[i+1]+"</td><td>"+allStudent[i].firstname+"</td><td>"+allStudent[i].lastname+"</td><td>"+allStudent[i].age+"</td><td><button class='btn btn-danger' onclick='dele("+i+")'>delet</button></td><td><button class='btn btn-primary' onclick='edit("+i+")'>edit</button></td>"
-    //     // tit += `</tr>` 
-    //     console.log(NOTE[i])
-    // }
-    // notetitle.innerHTML = tit;
-    // notebody.innerHTML = bod;
+    details.push(informatio)
+    localStorage.member = JSON.stringify(details);
+
+    let t = "";
+    details = JSON.parse(localStorage.getItem("member"));
+    for (let i = 0; i < details.length; i++) {
+        t += `<tr>`
+        t += `<th>${details[i].title}</th>`
+        t += `</tr>`
+        t += `<tr>`
+        t += `<td>${details[i].note}</td>`
+        t += `</tr>`
+        console.log(details[i])
+    }
+    display.innerHTML = t;
 }
