022e908fb99f02335e2f09aaf26db3e7683546e9
debo <ogunweoluwadebo@gmail.com>
2022-07-03 16:38:17 +0100

    all done

--- note.js
+++ note.js
@@ -16,8 +16,8 @@ const save = () => {
 function show() {
     details = JSON.parse(localStorage.getItem("member"));
     let t = ""
-    let b = ""
-    for (let i = 0; i < details; i++) {
+    // let b = ""
+    for (let i = 0; i < details.length; i++) {
         t += `<h2 class="note_title" id="notetitle">${details[i].title}</h2>`
         t += `<p class="note_body" id="notebody">${details[i].note}</p>`
         t += `<button class="note_btn note_view">View Detail</button>
@@ -25,25 +25,5 @@ function show() {
         console.log(details[i])
     }
     display.innerHTML = t;
-    // details = JSON.parse(localStorage.getItem("member"));
-    // let t = "";
-    // for (let i = 0; i < details.length; i++) {
-    //     t += `<tr>`
-    //     t += `<th>${details[i].title}</th>`
-    //     t += `</tr>`
-    //     t += `<tr>`
-    //     t += `<td>${details[i].note}</td>`
-    //     t += `</tr>`
-    //     console.log(details[i])
-    // }
-    // display.innerHTML = t;
-    // details = JSON.parse(localStorage.getItem("member"));
-    // let t = ""
-    // for (let i = 0; i < details.length; i++) {
-    //     t += `<tr>`
-    //     t += `<td>${details[i].title}</td><td><button class='btn btn-danger' onclick='dele("+i+")'>delet</button></td><td><button class='btn btn-primary' onclick='edit("+i+")'>edit</button></td>`
-    //     t += `</tr>` 
-    //     console.log(details[i])
-    // }
-    // display.innerHTML = t;
+    // disp.innerHTML = b;
 }
