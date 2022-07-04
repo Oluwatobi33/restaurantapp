8b57e9169fe0db2862ca8a9cd2d099c5d1793604
debo <ogunweoluwadebo@gmail.com>
2022-07-04 19:43:40 +0100

    all note ready

--- note.js
+++ note.js
@@ -3,27 +3,49 @@ const save = () => {
     if (localStorage.member) {
         details = JSON.parse(localStorage.member)
     }
-    var userTitle = title.value;
-    var userNote = note.value;
+    var hod = title.value;
+    var uk = note.value;
     informatio = {
-        title:userTitle,
-        note:userNote,
+        ttle:hod,
+        bk:uk,
     }
     details.push(informatio)
     localStorage.member = JSON.stringify(details);
-    location.reload()
+    // location.reload()
 }
 function show() {
-    details = JSON.parse(localStorage.getItem("member"));
     let t = ""
-    // let b = ""
+    let b = ""
+    details = JSON.parse(localStorage.getItem("member"));
     for (let i = 0; i < details.length; i++) {
-        t += `<h2 class="note_title" id="notetitle">${details[i].title}</h2>`
-        t += `<p class="note_body" id="notebody">${details[i].note}</p>`
-        b += `<button class="note_btn note_view">View Detail</button>
-        </td><td><button class="note_btn note_delete">Delete Note</button></td>` 
-        console.log(details[i])
+        t += `<h2 class="note_title" id="notetitle">${details[i].ttle}</h2><p class="note_body" id="notebody">${details[i].bk}</p><button onclick='edit(${i})' class="note_btn note_view">Edit Detail</button>
+        </td><td><button onclick='remove(${i})' class="note_btn note_delete">Delete Note</button></td>` 
     }
+    console.log(details)
     display.innerHTML = t;
-    // disp.innerHTML = b;
 }
+function remove(i){
+    detail = JSON.parse(localStorage.getItem("member"));
+    detail.splice(i,1)
+    localStorage.setItem('member',JSON.stringify(detail))
+    location.reload()
+}
+// var edite;
+// function edit(i){
+//     detail = JSON.parse(localStorage.getItem("member"));
+//     console.log(detail);
+//     itle.value = detail[i].ttle;
+//     bk.value = detail[i].bk;
+//     hov.style.display="none";
+//     upd.style.display="block";
+//     edite = i;
+// }
+// function update(){
+//     details = JSON.parse(localStorage.getItem("member"))
+//     details.splice(edite,1,{
+//         ttle:itle.value,
+//         bk:bk.value
+//     })
+//     localStorage.member = JSON.stringify(details)
+//     window.location.reload()
+// }
