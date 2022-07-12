2a514d52ddb4bfc3b08c54d310137439e6c76a83
debo <ogunweoluwadebo@gmail.com>
2022-07-12 16:55:20 +0100

    note adjusted

--- note.js
+++ note.js
@@ -1,25 +1,37 @@
 tobi = JSON.parse(localStorage.getItem("project"));
 var loginuser = JSON.parse(localStorage.getItem("Eail"));
-let receiver;
+let hass;
 tobi.map((item,index)=>{
     if (tobi[index].Email == loginuser) {
-        receiver = tobi[index].note
+        hass = tobi[index].note;
     }
 })
+const only = ()=>{
+    for (var index = hass.length -1; index >= 0; index--) {
+        display.innerHTML += `<div class="container col-12 col-md-6 card my-2" style="background-color: #15273B;">
+            <h2 class="text-light">${hass[index].userTitle}</h2>
+            <p class="text-light">${hass[index].userNote}</p>
+            <div class="container pb-1">
+                <button class="btn me-3" onclick='del(${index})' style="background-color:#E01541;color:white;">Delet Note</button>
+                <button class="btn btn-primary me-3" onclick='edit(${index})' data-bs-toggle="modal" data-bs-target="#staticBackdrop">Edit Note</button>
+            </div>
+        </div>`
+    }
+}
 const save = ()=>{
     if (title.value != "" && note.value != ""){
         informatio = {  
             userTitle:title.value,
             userNote:note.value,
         }
-        receiver.push(informatio);
+        hass.push(informatio);
         display.innerHTML = "";
         only();
         title.value = "";
         note.value = "";
         tobi.map((item,index)=>{
-            if (tobi[index].userEmail == loginuser) {
-                tobi[index].userNote = receiver;
+            if (tobi[index].Email == loginuser) {
+                tobi[index].note = hass;
                 console.log(tobi[index].note);
             }
         })
@@ -28,26 +40,14 @@ const save = ()=>{
         alert("This is an empty note, it will not be saved")
     }
 }
-const only = ()=>{
-    for (var index = receiver.length -1; index >= 0; index--) {
-        display.innerHTML += `<div class="container col-12 col-md-6 card my-2" style="background-color: #15273B;">
-            <h2 class="text-light">${receiver[index].userTitle}</h2>
-            <p class="text-light">${receiver[index].userNote}</p>
-            <div class="container pb-1">
-                <button class="btn me-3" onclick='del(${index})' style="background-color:#E01541;color:white;">Delet Note</button>
-                <button class="btn btn-primary me-3" onclick='edit(${index})' data-bs-toggle="modal" data-bs-target="#staticBackdrop">Edit Note</button>
-            </div>
-        </div>`
-    }
-}
 const del = (index)=>{
     let answer = confirm("Are you sure, you want to delet this note.");
     if (answer === true) {      
-        let filteredArray = receiver.filter((item,ind)=>index != ind);
-        receiver = filteredArray;
+        let filteredArray = hass.filter((item,ind)=>index != ind);
+        hass = filteredArray;
         tobi.map((item,index)=>{
-            if (tobi[index].userEmail == loginuser) {
-                tobi[index].userNote = receiver;
+            if (tobi[index].Email == loginuser) {
+                tobi[index].note = hass;
                 console.log(tobi[index].note);
             }
         })
@@ -60,11 +60,11 @@ let ind;
 const edit = (index)=>{
     let answer = confirm("Are you sure, you want to edit this note. if yes, you can delet the previous one, if not needed.")
     if (answer === true) {        
-        title.value = receiver[index].userTitle;
-        note.value = receiver[index].userNote;
+        title.value = hass[index].userTitle;
+        note.value = hass[index].userNote;
         tobi.map((item,index)=>{
-            if (tobi[index].userEmail == loginuser) {
-                tobi[index].userNote = receiver;
+            if (tobi[index].Email == loginuser) {
+                tobi[index].note = hass;
                 console.log(tobi[index].note);
             }
         })
